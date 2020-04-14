bd4ba0476fed9463a69878ccde5287d4ab8871e3
lbyjwwyqt147 <ljy671018>
2020-04-14 14:31:50 +0800

    认证

--- src/main/java/pers/liujunyi/cloud/signature/autoconfigure/RestTemplateConfig.java
+++ src/main/java/pers/liujunyi/cloud/signature/autoconfigure/RestTemplateConfig.java
@@ -1,6 +1,5 @@
 package pers.liujunyi.cloud.signature.autoconfigure;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +15,7 @@ public class RestTemplateConfig {
      * @return
      */
     @Bean
-    @LoadBalanced
+   // @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
